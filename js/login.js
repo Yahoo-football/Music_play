83509bf0b1139e8334626b8017d5df73272f60a6
KUNTHEA.YON <KUNTHEA.YON@pnc.local>
2026-01-04 11:30:56 +0700

    bugfix:update music

--- js/login.js
+++ js/login.js
@@ -12,31 +12,32 @@ const name = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const btn = document.getElementById("btn");
-const text= document.querySelector('#text')
-let count=0;
+const text = document.querySelector('#text')
+let count = 0;
 
 const login = document.getElementById("login");
-const aTag= login.querySelector('a')
-const userName= document.getElementById('username')
-const userPassword =document.getElementById('userpassword')
+const aTag = login.querySelector('a')
+const userName = document.getElementById('username')
+const userPassword = document.getElementById('userpassword')
 
-// let user = [];
-// localStorage.setItem('user',JSON.stringify(user));
 
-login.addEventListener('click',()=>{
+login.addEventListener('click', () => {
   const user = localStorage.getItem("user");
-  for (let data of JSON.parse(user) ){
+  for (let data of JSON.parse(user)) {
     console.log(data.password)
-    console.log(userPassword.value===data.password)
-    if (userPassword.value===data.password && userName.value===data.name || userName.value===data.email ){
-      aTag.href='home.html';
-  }
+    console.log(userPassword.value === data.password)
+    if (userPassword.value === data.password && userName.value === data.name || userName.value === data.email) {
+      aTag.href = 'home.html';
+    }
   }
 })
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
-  if(count>=8){
+
+  let user = [];
+  localStorage.setItem('user', JSON.stringify(user));
+  if (count >= 8) {
     const user = localStorage.getItem("user");
     let userAppend = JSON.parse(user);
     userAppend.forEach((u) => {
@@ -49,34 +50,34 @@ btn.addEventListener("click", (e) => {
       email: email.value,
       password: password.value,
     };
-  
+
     console.log(newUser);
     userAppend.push(newUser);
-  
+
     localStorage.setItem("user", JSON.stringify(userAppend));
-  
+
     name.value = "";
     email.value = "";
     password.value = "";
-    count=0;
-    text.style.color='white'
+    count = 0;
+    text.style.color = 'white'
     container.classList.remove("right-panel-active");
   }
 });
 
-password.addEventListener('input',()=>{
-      if(event.data===null){
-        count-=1
-      }
-      else{
-        count+=1
-      }
-      if (count<=7 && count>=0){
-        text.style.color='red';
-      }
-      else{
-        text.style.color='green';
-      }
+password.addEventListener('input', () => {
+  if (event.data === null) {
+    count -= 1
+  }
+  else {
+    count += 1
+  }
+  if (count <= 7 && count >= 0) {
+    text.style.color = 'red';
+  }
+  else {
+    text.style.color = 'green';
+  }
 })
 
 
@@ -87,28 +88,28 @@ openEyeR.addEventListener("click", () => {
   openEyeR.style.display = "none";
   closeEyeR.style.display = "block";
   for (let show of showPassword) {
-    show.type='text';
+    show.type = 'text';
   }
 });
 closeEyeR.addEventListener("click", () => {
   closeEyeR.style.display = "none";
   openEyeR.style.display = "block";
   for (let show of showPassword) {
-    show.type='password';
+    show.type = 'password';
   }
 });
 openEyeL.addEventListener("click", () => {
   openEyeL.style.display = "none";
   closeEyeL.style.display = "block";
   for (let show of showPassword) {
-    show.type='text';
+    show.type = 'text';
   }
 });
 closeEyeL.addEventListener("click", () => {
   closeEyeL.style.display = "none";
   openEyeL.style.display = "block";
   for (let show of showPassword) {
-    show.type='password';
+    show.type = 'password';
   }
 });
 
